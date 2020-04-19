9d1c4d140d59cb6221f117667e3465635c38ee18
Kevin Patel <kevinjaypatel@gmail.com>
2020-04-19 11:43:53 -0700

    Added methods for standards and students in the Business Layer interface.

--- Mm.BusinessLayer/IBusinessLayer.cs
+++ Mm.BusinessLayer/IBusinessLayer.cs
@@ -5,20 +5,43 @@ namespace Mm.BusinessLayer
 {
     public interface IBusinessLayer
     {
+        #region Teacher
         IList<Teacher> GetAllTeachers();
         Teacher GetTeacherByName(string teacherName);
-        Teacher GetTeacherById(int teacherID);
+        Teacher GetTeacherById(int teacherId);
         void AddTeacher(params Teacher[] teachers);
         void UpdateTeacher(params Teacher[] teachers);
         void RemoveTeacher(params Teacher[] teachers);
+        #endregion
 
+        #region Course
         IList<Course> GetCoursesByTeacherId(int teacherId);
-
         IList<Course> GetAllCourses();
         Course GetCourseByName(string courseName);
         Course GetCourseById(int courseId);
         void AddCourse(params Course[] courses);
         void UpdateCourse(params Course[] courses);
         void RemoveCourse(params Course[] courses);
+        #endregion
+
+        #region Standard
+        IList<Standard> GetAllStandards();
+        Standard GetStandardByID(int standardId);
+        Standard GetStandardByName(string standardName);
+        void AddStandard(params Standard[] standards);
+        void UpdateStandard(params Standard[] standards);
+        void RemoveStandard(params Standard[] standards);
+        #endregion
+
+        #region Student
+        IList<Student> GetAllStudents();
+        Student GetStudentByName(string studentName);
+        Student GetStudentById(int studentId);
+        void AddStudent(params Student[] students);
+        void UpdateStudent(params Student[] students);
+        void RemoveStudent(params Student[] students); 
+        #endregion
+
+
     }
 }
